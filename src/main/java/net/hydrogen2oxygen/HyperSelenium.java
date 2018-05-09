001e7cb6d0a92a3cae9dbe5321b0d05a02ad4111
hydrogen2oxygen <luxuspeter@googlemail.com>
2018-05-09 15:51:48 +0200

    Java 1.8 in pom.xml and HelloWorld JavaFX (first time, yeah)

--- src/main/java/net/hydrogen2oxygen/HyperSelenium.java
+++ src/main/java/net/hydrogen2oxygen/HyperSelenium.java
@@ -1,8 +1,15 @@
 package net.hydrogen2oxygen;
 
+import net.hydrogen2oxygen.configuration.RunArguments;
+
 public class HyperSelenium {
+
+    private static RunArguments runArguments;
 
     public static void main(String [] args) {
         System.out.println("Welcome to HyperSelenium!");
+
+        runArguments = new RunArguments(args);
+        System.out.println(runArguments.getKeyValues().toString());
     }
 }
