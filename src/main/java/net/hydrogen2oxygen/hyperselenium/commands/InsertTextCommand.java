bd3c2650c39ff6222adc2041c8c99714766515c6
hydrogen2oxygen <luxuspeter@googlemail.com>
2020-08-13 17:43:44 +0200

    Command text throw exception

--- src/main/java/net/hydrogen2oxygen/hyperselenium/commands/InsertTextCommand.java
+++ src/main/java/net/hydrogen2oxygen/hyperselenium/commands/InsertTextCommand.java
@@ -2,15 +2,20 @@ package net.hydrogen2oxygen.hyperselenium.commands;
 
 import net.hydrogen2oxygen.hyperselenium.domain.CommandResult;
 import net.hydrogen2oxygen.hyperselenium.domain.HyperseleniumCommand;
+import net.hydrogen2oxygen.hyperselenium.exceptions.CommandExecutionException;
 import net.hydrogen2oxygen.hyperselenium.selenium.HyperWebDriver;
 
 @HyperseleniumCommand
 public class InsertTextCommand extends BaseCommand {
 
     @Override
-    public CommandResult executeCommand(HyperWebDriver driver, String[] params) {
+    public CommandResult executeCommand(HyperWebDriver driver, String[] params) throws CommandExecutionException {
 
         CommandResult commandResult = new CommandResult();
+
+        if (params.length < 2) {
+            throw new CommandExecutionException("ERROR for 'text', not enough params!");
+        }
 
         driver.insertText(params[0],params[1]);
 
