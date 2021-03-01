d05ef3e7ce5ccc8c148a8dba1d600e7513065c53
hydrogen2oxygen <luxuspeter@googlemail.com>
2021-03-01 09:18:01 +0100

    ng-bootstrap

--- hyperselenium-webextension/src/app/app.module.ts
+++ hyperselenium-webextension/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
